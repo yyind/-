42ccb2a4ea95f45a53200595dcc142829210c6ff
yyind <110708416+yyind@users.noreply.github.com>
2022-09-17 13:39:48 +0900

    Update 1330.py

--- 1330.py
+++ 1330.py
@@ -1,9 +1,9 @@
-A,B=input().split()
-A=int(A)
-B=int(B)
-if(A>B):
- print(">")
-if(A<B):
- print("<")
-if(A==B):
- print("==")
+A,B=input().split() # A와 B를 쪼갬
+A=int(A) # A는 정수
+B=int(B) # B는 정수
+if(A>B): # A가 B보다 클 때
+ print(">") # > 출력
+if(A<B): # A가 B보다 작을 떄
+ print("<") # < 출력
+if(A==B): # A와 B가 같을 때
+ print("==") # ==출력
