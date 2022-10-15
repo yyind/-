2d7557effb51b9ad9485852beb3e1c8f9f25ec14
Jun <110708416+yyind@users.noreply.github.com>
2022-10-15 13:15:16 +0900

    Create 10039.py

---
+++ 10039.py
@@ -0,0 +1,40 @@
+a=int(input())
+if a>=40:
+ a = a
+
+else: 
+  a = 40
+
+
+b=int(input())
+if b>=40:
+ b = b
+
+else: 
+  b = 40
+
+
+c=int(input())
+if c>=40:
+ c = c
+
+else: 
+  c = 40
+
+
+d=int(input())
+if d>=40:
+ d = d
+
+else: 
+  d = 40
+
+
+e=int(input())
+if e>=40:
+ e = e
+
+else: 
+  e = 40
+
+print(int((a+b+c+d+e)/5))
